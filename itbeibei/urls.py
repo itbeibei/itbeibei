9863259e74c8d75c27b3d6bac0e72f9eabd42057
bbright3493 <44704708@qq.com>
2017-09-12 17:01:55 +0800

    first commit

--- itbeibei/urls.py
+++ itbeibei/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from itbeibeiApp1 import views
 
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',views.home,name='home')
