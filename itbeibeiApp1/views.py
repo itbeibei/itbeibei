a3b6ea789dbff1cec17c2258eddee1725a63583c
bbright3493 <44704708@qq.com>
2017-09-12 15:17:11 +0800

    first commit

--- itbeibeiApp1/views.py
+++ itbeibeiApp1/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from models import *
 # Create your views here.
+
+
+
 def home(request):
     testname = test.objects.all()
     return render(request,'home.html',locals())
